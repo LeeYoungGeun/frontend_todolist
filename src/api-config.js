75b79c7fd74de49850cc7a0eff5038794f385180
LeeYoungGeun <dudrms7863@nate.com>
2024-08-02 07:23:15 +0000

    frontend deployment

--- src/api-config.js
+++ src/api-config.js
@@ -6,7 +6,8 @@ if(hostname === "localhost") {
   backendHost = "http://localhost:8080";
 }else{
   // backendHost = "http://todoapplication00-backend0-dev.ap-northeast-2.elasticbeanstalk.com";
-  backendHost = "http://dev.ygyg.kro.kr";
+  // backendHost = "http://dev.ygyg.kro.kr";
+  backendHost = "https://dev.ygyg.kro.kr";
 }
 
 export const API_BASE_URL = `${backendHost}`;
